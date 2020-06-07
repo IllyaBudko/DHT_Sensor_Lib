0d0b35763a3a2164529099034cc60675241a4e4c
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2020-06-07 17:25:31 -0600

    commit

--- DHT_Sensor_Lib.h
+++ DHT_Sensor_Lib.h
@@ -16,37 +16,10 @@ typedef enum
   DHT_ERROR_Checksum
 }DHT_State_t;
 
-
-
-
-
 DHT_State_t DHT_Raw_Read(uint8_t Data[4]);
 
-
-
 void uS_Delay(uint16_t uSeconds);
-
 void GPIO_setInput(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin);
 void GPIO_setOutput(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif /*__DHT_SENSOR_LIB_H*/
