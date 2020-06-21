7eb428544445fd25f30810c12715f133f7b36beb
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2020-06-20 20:49:00 -0600

    commit

--- DHT_Sensor_Lib.h
+++ DHT_Sensor_Lib.h
@@ -18,28 +18,10 @@ typedef enum
   DHT_ERROR_Checksum
 }DHT_State_t;
 
-typedef struct
-{
-  DHT_State_t        DHT_State;
-  GPIO_InitTypeDef  *DHT_Pin;
-  TIM_HandleTypeDef *DHT_uS_Timer;
-  uint8_t            DHT_Raw_Data[4];
-  uint8_t            DHT_Temp_Integer;
-  uint8_t            DHT_Temp_Decimal;
-  uint8_t            DHT_RH_Integer;
-  uint8_t            DHT_RH_Decimal;
-}DHT_HandleTypeDef_t;
-
 DHT_State_t DHT_Raw_Read(uint8_t Data[4]);
 __weak void DHT_Error_Handler(DHT_State_t State);
 
-void uS_Delay(uint16_t uSeconds);
+void uS_Delay(uint16_t uSeconds,TIM_HandleTypeDef dht_tim);
 void GPIO_setInput(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin);
 void GPIO_setOutput(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin);
-
-
-//////////////////////////////////////////////////// Structure APIs //////////////////////////////////////////////////////////////
-
-DHT_State_t DHT_Input_Handler(DHT_HandleTypeDef_t DHT_Handle);
-
 #endif /*__DHT_SENSOR_LIB_H*/
